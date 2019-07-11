b0d1dce36ea529c98b2397b8f4e2b5ca951789fa
ljh <ljhvip8090s@163.com>
2019-07-11 20:42:43 +0800

    基于axios封装request请求模块

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
-  },
-  'postcss-pxtorem': {
-    rootValue: 37.5,
-    propList: ['*']
+const path = require('path')
+
+module.exports = (data) => {
+// 如果是 node_modules/vant 相关的文件，则将rootValue 设置为37.5，否则都设置为75
+  const rootValue = data.file.dirname.indexOf(`node_modules${path.sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
