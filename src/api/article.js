7e9ca0361353c80bcc166843138f63443dad7706
ljh <ljhvip8090s@163.com>
2019-07-14 21:11:14 +0800

    首页文章列表-封装api请求

--- src/api/article.js
+++ src/api/article.js
@@ -1,1 +1,18 @@
 // 封装文章相关接口请求函数
+import request from '@/utils/request'
+// 获取频道新闻推荐数据
+export const getArticles = ({
+  channelId, // 频道
+  timestamp, // 时间戳
+  withTop // 是否包含定置文章
+}) => {
+  return request({
+    methods: 'GET',
+    url: '/app/v1_1/articles',
+    params: {
+      channel_id: channelId,
+      timestamp,
+      with_top: withTop
+    }
+  })
+}
